78adf1d70260324f5607e7d6e7f8531033a170b3
faisal-saddique <faisalsaddique7964@gmail.com>
2023-10-31 22:28:02 +0500

    added persona-forge

---
+++ pages/PersonaForge.py
@@ -0,0 +1,58 @@
+from dotenv import load_dotenv
+import streamlit as st
+import datetime
+from utilities.utils import (
+    get_initial_persona_from_articles,
+    apply_persona_to_article
+)
+
+# Set Streamlit page configuration
+st.set_page_config(
+    page_title='PersonaForge',
+    page_icon='🧙‍♂️',
+    layout='wide',
+    initial_sidebar_state='expanded'
+)
+
+# Main title
+st.title("PersonaForge - Craft Your Writing Style 🧙‍♂️📖")
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Input fields and labels
+person_name = st.text_input(
+    "Enter the name of the person you want to emulate:")
+articles = st.text_area(
+    "Enter the articles written by them (max 1500 words):", height=400)
+raw_article = st.text_area(
+    f"Enter the raw article to convert to the subject's style:", height=400)
+
+# Error handling and button label
+if st.button("Generate", use_container_width=True):
+    if person_name and articles and raw_article:
+        if "created_persona" not in st.session_state:
+            # Generate initial persona
+            with st.expander("Creating Persona"):
+                res_box = st.empty()
+                system_prompt_for_generation_step = get_initial_persona_from_articles(
+                    person_name, articles, res_box)
+                st.session_state["created_persona"] = system_prompt_for_generation_step
+        else:
+            system_prompt_for_generation_step = st.session_state["created_persona"]
+        # Generate refined article
+        st.subheader("Refined Article")
+        final_res_box = st.empty()
+        final_article = apply_persona_to_article(raw_article=raw_article, person=person_name,
+                                                    original_articles=articles, persona_prompt=system_prompt_for_generation_step,
+                                                    container=final_res_box)
+        if final_article:
+            # Download button
+            current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            sanitized_datetime = current_datetime.replace(":", "-")
+            file_name = f"{person_name}_styled_article_{sanitized_datetime}.md"
+            st.download_button(label="Download Refined Article", data=final_article, file_name=file_name, mime="text/markdown",
+                                use_container_width=True)
+    else:
+        st.warning(
+            "Please fill in all required fields: Person name, articles, and raw article.")
--- utilities/utils.py
+++ utilities/utils.py
@@ -9,6 +9,91 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+def apply_persona_to_article(raw_article, person, original_articles, persona_prompt, container):
+    tokens = num_tokens_from_string(raw_article)
+    container.info(f"No. of Tokens in the raw article: {tokens}")
+    if tokens > 1000:
+        # Consider the first 7500 tokens of content
+        container.info("Chunking to put it under the limit.")
+        raw_article = raw_article[:700]
+
+    tokens = num_tokens_from_string(original_articles)
+    container.info(f"No. of Tokens in the original articles: {tokens}")
+    if tokens > 2000:
+        # Consider the first 7500 tokens of content
+        container.info("Chunking to put it under the limit.")
+        original_articles = original_articles[:1800]
+
+    report = []
+    for resp in openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system",
+                "content": persona_prompt
+            },
+            {
+                "role": "user",
+                "content": f"Here are a few articles written by {person}:\n\n{original_articles}\n\n\nPlease now edit the following article as if it was written by {person}. Watch closely how he writes and then replicate his writing style so that you don't loose the essence of his persona. Here is the article to be refined:\n\n{raw_article}"
+            }
+        ],
+        temperature=.7,
+        max_tokens=4000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stream=True
+    ):
+        if "content" in resp.choices[0].delta:
+            report.append(resp.choices[0].delta.content)
+            result = "".join(report)      
+            container.markdown(result)
+
+    return "".join(report)
+
+def get_initial_persona_from_articles(person, content, container):
+    tokens = num_tokens_from_string(content)
+    container.info(f"No. of Tokens in the prompt: {tokens}")
+    if tokens > 2000:
+        # Consider the first 7500 tokens of content
+        container.info("Chunking to put it under the limit.")
+        content = content[:1500]
+        
+    SYSTEM_PROMPT = f"""Analyze {person}'s Articles and Craft His Persona in 200 Words!
+
+Your task: Delve deep into {person}'s articles with a sharp focus! Examine his writing style closely, break down his sentences, and closely examine the structure of his content. Your objective is to create a persona that mirrors {person}'s unique writing style. Pay close attention to the core of his writing; your designated writer must flawlessly emulate {person}'s voice.
+
+In less than 200 words, provide a thorough analysis and breakdown of {person}'s writing style. Leave no detail unturned, capturing every nuance that sets {person}'s writing apart. Remember, this isn't just about writing about {person}; you're composing a manual to help someone embody {person}'s writing style. Make this guide clear and to the point; it should serve as a step-by-step instruction for becoming {person} in his absence. Prepare to analyze, deconstruct, and replicate – the world craves more of {person}!
+
+Begin your analysis with the sentence by asking the person to be {person}, then proceed to dissect and detail the rest of your analysis"""
+
+    report = []
+    for resp in openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+            },
+            {
+                "role": "user",
+                "content": f"Here are a few articles written by {person}:\n\n{content}"
+            }
+        ],
+        temperature=.7,
+        max_tokens=5000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stream=True
+    ):
+        if "content" in resp.choices[0].delta:
+            report.append(resp.choices[0].delta.content)
+            result = "".join(report)      
+            container.markdown(result)
+
+    return "".join(report)
+
 def process_content_and_get_gpt_response(content, container):
     tokens = num_tokens_from_string(content)
     container.info(f"No. of Tokens in the prompt: {tokens}")
